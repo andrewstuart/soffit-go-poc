4160c1872fa858330453c2e120d3da547d96558d
Andrew Stuart <andrew.stuart2@gmail.com>
2016-07-30 12:09:42 -0700

    Update tests

--- jwk_test.go
+++ jwk_test.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"testing"
+
+	soffit "astuart.co/soffit-go"
 )
 
 func TestGen(t *testing.T) {
-	_, err := getJWT(Payload{}, "foo")
+	_, err := getJWT(soffit.Payload{}, "foo")
 
 	if err != nil {
 		t.Fatal(err)
